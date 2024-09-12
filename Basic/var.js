fad10c34f33135c66f190a21108fc1a8bc5406af
Abhishekkumar990 <abhishekkumar.sharma.cs.2020@mitmeerut.ac.in>
2024-09-13 00:34:07 +0530

    commit

--- Basic/var.js
+++ Basic/var.js
@@ -1,19 +1,19 @@
-const account = "account@gmail.com"
-var email = "Abhishek"
-let number = 1234567890
+const account = "account@gmail.com";
+var email = "Abhishek";
+let number = 1234567890;
 
-console.log(account)
-console.log(email)
-console.log(number)
+console.log(account);
+console.log(email);
+console.log(number);
 
-console.table([account, email, number])
+console.table([account, email, number]);
 
 // const account = 'asdfg'
-number = 0987654321
-email = "asdfg"
+number = 987654321;
+email = "asdfg";
 
-console.log(account)
-console.log(email)
-console.log(number)
+console.log(account);
+console.log(email);
+console.log(number);
 
-console.table([account, email, number])
+console.table([account, email, number]);
