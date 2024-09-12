333f5f2932e1d11d6d0b0c159a8f02242954cc39
Abhishekkumar990 <abhishekkumar.sharma.cs.2020@mitmeerut.ac.in>
2024-09-13 00:44:45 +0530

    data type02

---
+++ Basic/02datatype.js
@@ -0,0 +1,11 @@
+"use strict"; //use as the newer version of js code in my search engin.
+
+// alert('hello worl'); this is using for the window function.
+
+
+let a = 15;
+let b = 25;
+
+let add = a + b;
+let min = b - a;
+console.table([add, min]);
