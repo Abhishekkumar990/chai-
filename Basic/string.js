56ec8e1c36b7ee8a1eb9b3cb43e8c372d1b027b6
Abhishekkumar990 <abhishekkumar.sharma.cs.2020@mitmeerut.ac.in>
2024-09-15 23:18:29 +0530

    string final

---
+++ Basic/string.js
@@ -0,0 +1,13 @@
+let name = "Abhishek";
+let repoCount = 10;
+
+console.log(`this the name of ${name} and its user repoCount is ${repoCount}`);
+
+const gameName = new String("Abhishek")
+console.log(gameName)
+console.log(gameName[2])
+
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt()) //to find the number of index number
+
+console.log(gameName.indexOf('h'))
