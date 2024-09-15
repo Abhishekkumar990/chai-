b7b64a5dba65292668a543522c140f8a9ac56859
Abhishekkumar990 <abhishekkumar.sharma.cs.2020@mitmeerut.ac.in>
2024-09-15 19:51:38 +0530

    final op

---
+++ Basic/op.js
@@ -0,0 +1,42 @@
+// console.log(2 + 2)
+// console.log(3 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3)
+// console.log(3 / 2)
+// console.log(2 % 3)
+
+
+let str1 = "Abhishek"
+let str2 = "Sharma"
+
+let strR = str1 + str2
+
+console.log(strR);
+
+console.log("1" + "2")
+console.log("1" + 2)
+console.log("1" + 1 + 1)
+console.log(2 + 2 + "2")
+
+console.log((3 + 2) * 2 / 2)
+
+// console.log(+true)
+// console.log(true + ) this code not work
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+
+//pre-fix increamental and post-fix increamental
+
+const a = 10
+
+let b = a++
+    console.log(b)
+
+
+let c = ++a
+console.log(c)
+
+console.table([a, b, c])
