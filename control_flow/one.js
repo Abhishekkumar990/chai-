ecc8719eb959c7f63d6f172e2890913215fde318
Abhishekkumar990 <abhishekkumar.sharma.cs.2020@mitmeerut.ac.in>
2024-09-21 22:33:07 +0530

    conditional code

--- control_flow/one.js
+++ control_flow/one.js
@@ -3,3 +3,46 @@
 
 
 // if (condesion) {}
+
+const bal = 1000;
+
+// if (bal > 500){
+// console.log("this balance is good");
+
+// }
+
+//this is old or we can say the basic type of code 
+
+
+if (bal > 500) console.log("test");
+
+//this impecest type of code
+
+const b = 1000;
+
+// if (b > 500) {
+//     console.log("test");
+
+// } else if (b < 750) {
+//     console.log();
+
+// } else {
+//     console.log();
+// }
+
+
+const userloggedIN = true
+const debitCard = true
+
+if (condition && condition) {}
+
+// if (userloggedIN && debitCard) {
+//     console.log("Allow to buy course");
+// }
+
+const goolgeId = false;
+const email = true;
+
+// if (goolgeId || email) {
+//     console.log("user logged in");
+// }
