ada6f2450080b44f95efcdb833bd323c36b46040
Abhishekkumar990 <abhishekkumar.sharma.cs.2020@mitmeerut.ac.in>
2024-09-21 22:54:20 +0530

    loop

---
+++ iteration/one.js
@@ -0,0 +1,13 @@
+// for loop iteratioon in loop code
+
+//structur
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }
+
+let a;
+for (a = 1; a <= 10; a++) {
+    let b = a;
+    console.log(b);
+}
