a37cf0068515f3d6bfb0949614acc39e4483d1c1
Abhishekkumar990 <abhishekkumar.sharma.cs.2020@mitmeerut.ac.in>
2024-09-22 00:22:46 +0530

    do while and while loop

---
+++ iteration/two.js
@@ -0,0 +1,33 @@
+// In this code file learn about the while loop.
+//str.
+// while (condition) {
+
+// }
+
+
+let a = 0;
+while (a <= 10) {
+    //console.log(`value of a is ${a}`);
+    a = a + 2;
+}
+
+
+
+//THis code is use to provide the expamle of array loops fo while
+let myArray = ["Abhishek", "Rahul", "sonu", "Kunal", "Kundan"]
+    //console.log(myArray.length);
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+
+//Do-While Loops
+
+let s = 1;
+do {
+    console.log(`s is ${s}`);
+    s++
+}
+while (s < 10);
